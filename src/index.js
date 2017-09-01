dc56dfb95480736e6a47301ad5447b8cc494893f
Shane Ahern <shane@shaneahern.com>
2017-09-01 13:30:01 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Slider from 'react-toolbox/lib/slider';
+import styles from './VASSlider.css';
+
+
+export default class VASSlider extends React.Component {
+  static propTypes = {
+    maxRateDescription: PropTypes.string.isRequired,
+    minRateDescription: PropTypes.string.isRequired,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    theme: PropTypes.shape({}),
+    onRatingChange: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    min: 0,
+    max: 10,
+    step: 0.0001,
+    theme: styles,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0,
+    };
+  }
+  onChange = (rating) => {
+    this.setState({ rating }, this.props.onRatingChange(rating));
+  }
+
+  render() {
+    const { min, max, step, minRateDescription, maxRateDescription, theme } = this.props;
+    const { rating } = this.state;
+
+    return (
+      <div>
+        <div className={styles.vasRatingContainer}>
+          <div className={styles.start}>{minRateDescription}</div>
+          <div className={styles.main} />
+          <div className={styles.end}>{maxRateDescription}</div>
+        </div>
+        <div className={styles.vasRatingContainer}>
+          <div className={styles.main}>
+            <Slider
+              min={min}
+              max={max}
+              step={step}
+              value={rating}
+              onChange={this.onChange}
+              theme={theme}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'lib');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const WebpackConfig = {
+
+    entry: APP_DIR + '/index.js',
+
+    output: {
+        path: BUILD_DIR,
+        filename: 'index.js',
+        libraryTarget: 'umd',
+        library: 'CodeHighlight'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                include : APP_DIR,
+                options: {
+                    presets: [ 'env', 'react', 'es2015', 'stage-2' ]
+                }
+            },
+            { test: /\.css$/, loader: "style-loader!css-loader?modules" },
+            { test: /\.svg/, loader: 'svg-url-loader' },
+        ],
+    },
+}
+
+
+
+// webpack production config.
+if ( process.env.NODE_ENV === 'production' ) {
+
+    WebpackConfig.externals = {
+        'react': 'react',
+        'react-dom': 'react-dom',
+        'highlight.js': 'highlight.js'
+    };
+
+    WebpackConfig.plugins = [
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+            },
+            compress: {
+                warnings: false,
+                screw_ie8: true
+            },
+            comments: false
+        }),
+    ];
+
+}
+
+
+module.exports = WebpackConfig;
