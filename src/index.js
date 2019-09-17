dfe9262b28f3946f685a4ba6d2f84c9735199471
Jonathan Chang <slycoder@gmail.com>
2019-09-17 09:04:10 -0700

    Support rateValues (#4)
    * Updates to the latest version of react toolbox.
    * Respect rateValues by having the VAS interpolate the endpoints.
    * Allow the widget to be disabled.
    * Add an option to show the selected value.
    * Various styling tweaks.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Slider from 'react-toolbox/lib/slider';
 import styles from './VASSlider.css';
 
 
-export default class VASSlider extends React.Component {
+export default class VASSlider extends React.PureComponent {
   static propTypes = {
     maxRateDescription: PropTypes.string.isRequired,
     minRateDescription: PropTypes.string.isRequired,
@@ -12,6 +12,9 @@ export default class VASSlider extends React.Component {
     max: PropTypes.number,
     step: PropTypes.number,
     theme: PropTypes.shape({}),
+    disabled: PropTypes.bool,
+    rateValues: PropTypes.array,
+    showValue: PropTypes.bool,
     onRatingChange: PropTypes.func.isRequired,
   }
 
@@ -20,34 +23,87 @@ export default class VASSlider extends React.Component {
     max: 10,
     step: 0.0001,
     theme: styles,
+    disabled: false,
+    showValue: false,
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      rating: this.props.rating || 0,
+      rating: typeof this.props.rating === "number" ?
+        this.translateRating(this.props.rating) :
+        0.0,
     };
   }
+
   onChange = (rating) => {
-    this.setState({ rating }, this.props.onRatingChange(rating));
+    this.setState(
+      { rating },
+      this.props.onRatingChange(this.translatePosition(rating))
+    );
+  }
+
+  getRateValues = () => {
+    const {
+      min,
+      max,
+      minRateDescription,
+      maxRateDescription,
+      rateValues,
+    } = this.props;
+    const sanitizedRateValues = !rateValues || rateValues.length < 2 ?
+      [{ value: min, text: minRateDescription }, { value: max, text: maxRateDescription }] :
+      rateValues;
+
+    const head = sanitizedRateValues[0];
+    const tail = sanitizedRateValues[sanitizedRateValues.length - 1];
+    return {
+      head: {
+        text: head.text,
+        value: parseFloat(head.value),
+      },
+      tail: {
+        text: tail.text,
+        value: parseFloat(tail.value),
+      },
+    };
+  }
+
+  translatePosition = (position) => {
+    const { head, tail } = this.getRateValues();
+    return (tail.value - head.value) * position + head.value;
+  }
+
+  translateRating = (rating) => {
+    const { head, tail } = this.getRateValues();
+    return (rating - head.value) / (tail.value - head.value);
   }
 
   render() {
-    const { min, max, step, minRateDescription, maxRateDescription, theme } = this.props;
+    const {
+      step,
+      theme,
+      disabled,
+      showValue,
+    } = this.props;
     const { rating } = this.state;
+    const { head, tail } = this.getRateValues();
 
     return (
       <div>
         <div className={styles.vasRatingContainer}>
-          <div className={styles.start}>{minRateDescription}</div>
-          <div className={styles.main} />
-          <div className={styles.end}>{maxRateDescription}</div>
+          <div className={styles.start}>{head.text}</div>
+          <div className={styles.value}>
+            {showValue && this.translatePosition(rating)}
+          </div>
+          <div className={styles.end}>{tail.text}</div>
         </div>
         <div className={styles.vasRatingContainer}>
           <div className={styles.main}>
             <Slider
-              min={min}
-              max={max}
+              disabled={disabled}
+              min={0}
+              max={1}
               step={step}
               value={rating}
               onChange={this.onChange}
